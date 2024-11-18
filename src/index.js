94ec9ceee03020ef986f2b5272bca2c938ddccc2
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2024-11-18 14:33:12 -0700

    routing setup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './pages/css/index.css';
 import App from './pages/App.jsx';
+// Import necessary components for routing
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//routing system configuration
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  // Add more routes as needed
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* routing implemented */}
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
