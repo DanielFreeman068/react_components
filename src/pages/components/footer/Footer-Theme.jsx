a1aa5a72bc02f89efdfc70bd4f0fe42825f57f51
DanielFreeman068 <141285622+DanielFreeman068@users.noreply.github.com>
2024-11-20 18:53:15 -0700

    footer component done

--- src/pages/components/footer/Footer-Theme.jsx
+++ src/pages/components/footer/Footer-Theme.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react';
 import style from './Footer.css'
 import { FaGithub, FaLinkedin, FaInstagram, FaFacebook } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
-function Footer_Theme({logoImage, about, faqs, home, contact, socialLinks}) {
-    const [isOpen, setIsOpen] = useState(false);
+function Footer_Theme({logoImage, routes, socialLinks, webName, address}) {
     const iconStyle = {
         width: "25px",
         height: "25px",
@@ -13,7 +11,52 @@ function Footer_Theme({logoImage, about, faqs, home, contact, socialLinks}) {
     };
     return (
         <div className="footer-background" style={style}>
-            <h1>hi</h1>
+            <footer>
+        <div className="fcont">
+            <div className="fhead">
+            {!logoImage ? (<img src="https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg" alt="logo"/>) : (<img src={logoImage} alt="logo"/>)}
+            {address ? (<div className='ftxt'>{address}</div>) : null}
+            </div>
+            <div className="links">
+                <h1>Website Links</h1>
+                {routes.home? (<h3><a href={routes.home}>Home page</a></h3>) : null}
+                {routes.about? (<h3><a href={routes.about}>About Us</a></h3>) : null}
+                {routes.testimonials? (<h3><a href={routes.testimonials}>Testimonials</a></h3>) : null}
+                {routes.contact? (<h3><a href={routes.contact}>Contact Us</a></h3>) : null}
+                {routes.faqs? (<h3><a href={routes.faqs}>FAQS</a></h3>) : null}
+            </div>
+            <div className="socials">
+                <h1 className="socialtxt">
+                    Social Media
+                </h1>
+                <div className="social-imgs">
+                    {socialLinks?.github && (<a href={socialLinks.github} target="_blank" rel="noopener noreferrer"className="social-link">
+                        <FaGithub style={{ ...iconStyle, color: "#181717" }} />
+                    </a>
+                    )}
+                    {socialLinks?.twitter && (<a href={socialLinks.twitter} target="_blank" rel="noopener noreferrer" className="social-link">
+                        <FaXTwitter style={{ ...iconStyle, color: "#1DA1F2" }} />
+                    </a>
+                    )}
+                    {socialLinks?.linkedIn && (<a href={socialLinks.linkedIn} target="_blank" rel="noopener noreferrer" className="social-link">
+                        <FaLinkedin style={{ ...iconStyle, color: "#0077B5" }} />
+                    </a>
+                    )}
+                    {socialLinks?.instagram && (<a href={socialLinks.instagram} target="_blank"  rel="noopener noreferrer"className="social-link">
+                        <FaInstagram style={{...iconStyle,color: "#E4405F" }} />
+                    </a>
+                    )}
+                    {socialLinks?.facebook && (<a href={socialLinks.facebook} target="_blank" rel="noopener noreferrer"className="social-link">
+                        <FaFacebook style={{ ...iconStyle, color: "#1877F2" }} />
+                    </a>
+                    )}
+                </div>
+            </div>
+        </div>
+        <div className="foot">
+            All Rights Reserved | ©2024 {webName}
+        </div>
+    </footer>
         </div>
     )
 }
--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -1,18 +1,24 @@
 import Footer_Theme from './components/footer/Footer-Theme.jsx'
+
 function Footer(){
     // define variables
     let info = {
-        logoImage: "https://res.cloudinary.com/dehoobuso/image/upload/v1731377347/full-cloud-tasks/dog10.jpg.jpg",
-        about: "/about",
-        faqs: "/faqs",
-        home: "/",
-        contact: "/contact",
+        webName: "Cloud",
+        address: "123 West Streetington Arizona 85308",
+        logoImage: "https://res.cloudinary.com/dehoobuso/image/upload/v1730419145/samples/logo.png",
+        routes: {
+            about: "/about",
+            faqs: "/faqs",
+            home: "/",
+            testimonials: "/testimonials",
+            contact: "/profile-card",
+        },
         socialLinks: {
             github: "https://github.com/johndoe",
             instagram: "https://instagram.com/johndoe",
             facebook: "https://facebook.com/johndoe",
             twitter: "https://twitter.com/johndoe",
-            linkedIn: "https://linkedIn.com/johndoe"
+            linkedIn: "https://linkedIn.com/johndoe",
         },
     }
     return(
@@ -28,8 +34,9 @@ function Footer(){
         textDecoration: 'none', 
         boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
         }}>Return to Library</a>
-        
-        <Footer_Theme logoImage = {info.logoImage} about = {info.about} faqs = {info.faqs} home = {info.home} contact = {info.contact} socialLinks = {info.socialLinks}/>
+        <p style={{maxWidth:'200px', margin:'0 auto'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, mollitia id dolorum sint atque ipsa doloribus laboriosam, ad quae dignissimos corporis ipsam provident cum libero iste fugiat alias velit tenetur quia? Doloribus necessitatibus aliquam possimus dolor vel! Ullam exercitationem recusandae vitae voluptas animi aut enim voluptatibus debitis quas beatae nihil perspiciatis nemo, laboriosam totam maxime dolor? Quibusdam iusto labore eius quisquam corrupti dolore cupiditate doloribus vitae accusantium, totam quia, suscipit aut quasi eaque voluptatem velit veniam nam fugit facere sed ullam officiis ex ad quis! Animi vero neque fugiat nesciunt quos distinctio quisquam accusantium delectus! Rem recusandae, vel, labore officia itaque laudantium accusantium, asperiores esse saepe magnam error atque. Deserunt debitis consequatur in numquam velit nihil quaerat ut nulla. Maxime iste facere incidunt sint consequuntur, officia exercitationem molestias tempora reiciendis nostrum. Id esse nemo autem vitae. Nesciunt eveniet in illo, beatae,   
+        </p>
+        <Footer_Theme logoImage = {info.logoImage} routes = {info.routes} socialLinks = {info.socialLinks} webName = {info.webName} address = {info.address}/>
         </>
     )
 }
