743345e339bd51167a9e4655426ddcb1f100620c
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2024-11-21 13:37:04 -0700

    slight change

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     // define variables
     let info = {
         webName: "Cloud",
-        address: "123 West Streetington Arizona 85308",
+        address: "123 West Cloudington Arizona 85308",
         logoImage: "https://res.cloudinary.com/dehoobuso/image/upload/v1730419145/samples/logo.png",
         routes: {
             about: "/about",
@@ -34,8 +34,6 @@ function Footer(){
         textDecoration: 'none', 
         boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
         }}>Return to Library</a>
-        <p style={{maxWidth:'200px', margin:'0 auto'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, mollitia id dolorum sint atque ipsa doloribus laboriosam, ad quae dignissimos corporis ipsam provident cum libero iste fugiat alias velit tenetur quia? Doloribus necessitatibus aliquam possimus dolor vel! Ullam exercitationem recusandae vitae voluptas animi aut enim voluptatibus debitis quas beatae nihil perspiciatis nemo, laboriosam totam maxime dolor? Quibusdam iusto labore eius quisquam corrupti dolore cupiditate doloribus vitae accusantium, totam quia, suscipit aut quasi eaque voluptatem velit veniam nam fugit facere sed ullam officiis ex ad quis! Animi vero neque fugiat nesciunt quos distinctio quisquam accusantium delectus! Rem recusandae, vel, labore officia itaque laudantium accusantium, asperiores esse saepe magnam error atque. Deserunt debitis consequatur in numquam velit nihil quaerat ut nulla. Maxime iste facere incidunt sint consequuntur, officia exercitationem molestias tempora reiciendis nostrum. Id esse nemo autem vitae. Nesciunt eveniet in illo, beatae,   
-        </p>
         <Footer_Theme logoImage = {info.logoImage} routes = {info.routes} socialLinks = {info.socialLinks} webName = {info.webName} address = {info.address}/>
         </>
     )
