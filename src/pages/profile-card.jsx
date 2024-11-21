a8148d9f4f015b9d3293fe7b168d64cd2605236a
DanielFreeman068 <141285622+DanielFreeman068@users.noreply.github.com>
2024-11-21 16:12:24 -0700

    styled bio on profile card page and then fixed top page button on footer as well as glow effect on hover for social icons

--- src/pages/profile-card.jsx
+++ src/pages/profile-card.jsx
@@ -1,4 +1,4 @@
-import ProfileCard from './components/profile-card/Profile-card-component.jsx'
+import ProfileCard from './components/profile-card/profile-card-component.jsx'
 function Profile(){
     // define variables
     let info = {
