39195ca9adf540578fdfbca41f956ad68df2955d
DanielFreeman068 <141285622+DanielFreeman068@users.noreply.github.com>
2024-11-22 10:18:16 -0700

    nothing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './pages/css/index.css';
 import App from './pages/App.jsx';
 import Profile from './pages/profile-card.jsx'
 import Footer from './pages/footer.jsx'
@@ -22,7 +21,3 @@ root.render(
     <RouterProvider router={router}/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,3 +1,5 @@
+import './css/index.css'
+
 function App() {
   return (
     <div class="container">
