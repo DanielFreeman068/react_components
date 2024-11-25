7c8e390c815dc3234cac7e9900efc71c2dfcabb2
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2024-11-25 15:21:53 -0700

    library styling

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,13 +2,25 @@ import './css/index.css'
 
 function App() {
   return (
-    <div class="container">
-      <h1>React Components Library</h1>
+    <div class="library-container">
+      <h1 className='library-header'>React Components Library</h1>
       <div class="components-grid">
-        <div class="component-card"><a href="/profile-card">Profile Card</a></div>
-        <div class="component-card"><a href="/footer">Footer</a></div>
-        <div class="component-card"><a href="/gallery">Responsive Gallery</a></div>
-        <div class="component-card"><a href="/">TBD</a></div>
+        <a href="/profile-card" class="component-card">
+          <h1 className='library-nav-h1' href="/profile-card">Profile Card</h1>
+          <p className='library-components-desc'>React Component for a Profile Card</p>
+        </a>
+        <a href="/footer" class="component-card">
+          <h1 className='library-nav-h1' href="/footer">Footer</h1>
+          <p className="library-components-desc">React Component for a Footer</p>
+        </a>
+        <a href="/gallery" class="component-card">
+          <h1 className='library-nav-h1' href="/gallery">Responsive Gallery</h1>
+          <p className="library-components-desc">React Component for a Responsive Gallery</p>
+        </a>
+        <a href="/" class="component-card">
+          <h1 className='library-nav-h1' href="/">TBD</h1>
+          <p className="library-components-desc">React Component for who knows what</p>
+        </a>
       </div>
     </div>
   );
